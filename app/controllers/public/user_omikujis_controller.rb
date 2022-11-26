52dac17b1297fbb4f88337ad9bf0460a48ddc061
shoaws <108819031+shoaws@users.noreply.github.com>
2022-11-26 06:00:47 +0000

    [Fix]おみくじを1日１回に変更

--- app/controllers/public/user_omikujis_controller.rb
+++ app/controllers/public/user_omikujis_controller.rb
@@ -1,9 +1,17 @@
 class Public::UserOmikujisController < ApplicationController
   def create
-    omikuji = Omikuji.all.sample
     if user_signed_in?
-      user_omikuji = current_user.user_omikujis.new(omikuji_id: omikuji.id)
-      user_omikuji.save
+      if current_user.user_omikujis.created_today.exists?
+        today_user_omikuji = current_user.user_omikujis.created_today.order(created_at: :desc).first
+        omikuji = Omikuji.find_by(id: today_user_omikuji.omikuji_id)
+        flash[:notice] = '引けるのは1日に１回です！'
+      else
+        omikuji = Omikuji.all.sample
+        user_omikuji = current_user.user_omikujis.new(omikuji_id: omikuji.id)
+        user_omikuji.save
+      end
+    else
+      omikuji = Omikuji.all.sample
     end
     redirect_to omikuji_path(omikuji)
   end
