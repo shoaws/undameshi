62e86803820514aae494dbbc3ad83f4ee20f51bf
shoaws <108819031+shoaws@users.noreply.github.com>
2022-12-04 10:41:37 +0000

    [Add]ユーザー編機能

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -15,6 +15,19 @@ class Public::UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      bypass_sign_in(@user)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   private
